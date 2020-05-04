ee3fd37a681a44dec57d09e53e7b836f3d1defc9
Shivam Singhal <59584173+shivaamm@users.noreply.github.com>
2020-05-05 01:47:51 +0530

    Add files via upload

---
+++ covid_page/sample/index.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CoViD-19</title>
+    <?php include 'link/links.php'; ?>
+    <?php include 'css/style.php'; ?>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg nav_style p-3">
+  <a class="navbar-brand pl-5" href="#">CoViD-19</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto pr-5 text-capitalize">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#aboutid">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#sympid">Symptoms</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#preventid">Preventions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contactid">Contact Us</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+<div class="main_header">
+   <div class="row w-100 h-100">
+        <div class="col-lg-5 col-md-5 col-12 order-lg-1 order-2">
+            <div class="leftside w-100 h-100 d-flex justify-content-center align-items-center">
+                <img src="images/3.png" width="300" height="300">
+            </div>
+        </div>
+        <div class="col-lg-7 col-md-7 col-12 order-lg-2 order-1">
+            <div class="rightside w-100 h-100 d-flex justify-content-center align-items-center">
+                <h1> Let's Stay Safe & Fight Together Against Cor<span class="rotate"><img src="images/4.png" width="100" height="100"></span>na Virus</h1>
+            </div>
+        </div>
+</div>
+
+<!-- ********* corona updates *******-->
+<section class="updates">
+    <div class="mb-3">
+        <h3 class="text-uppercase text-center">Covid-19 Updates</h3>
+    </div>
+    <div class="d-flex justify-content-around align-items-center count_style">
+        <div>
+            <h1 class="count">3000000</h1>
+            <p> Passengers</p>
+        </div>
+        <div>
+            <h1 class="count">2500</h1>
+            <p> Active Cases</p>
+        </div>
+        <div>
+            <h1 class="count">500</h1>
+            <p> Recovered</p>
+        </div>
+        <div>
+            <h1 class="count">1500</h1>
+            <p> Deaths</p>
+        </div>
+</section>
+
+<!-- ******* about *******-->
+<div class="container-fluid sub_section pt-5 pb-5 " id="aboutid">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1> About Covid-19 </h1>
+    <div>
+    <div class="row pt-5">
+        <div class="col-lg-5 col-md-6 col-12 ml-5 about_res">
+            <img src="images/5.jpg" class="img-fluid">
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-12">
+            <h2>What is COVID-19 (Corona-Virus)</h2>
+            <p>Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+            This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019.</p>
+            <p>Corona viruses are a large family of viruses which may cause illness
+            in animals or humans. In humans, several coronaviruses are known
+            to cause respiratory infections ranging from the common cold to more
+            severe diseases such as Middle East Respiratory Syndrome (MERS)
+            and Severe Acute Respiratory Syndrome (SARS). The most recently
+            discovered coronavirus causes coronavirus disease COVID-19. </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid pt-5 pb-5 " id="sympid">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1> CoronaVirus Symptoms </h1>
+    <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/6.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Cold </figcaption>
+            </figure>
+            </div>
+
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/7.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Runny Nose </figcaption>
+            </figure>
+            </div>
+
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/8.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Fever </figcaption>
+            </figure>
+            </div>
+
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/9.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Cough </figcaption>
+            </figure>
+            </div>
+
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/10.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Tiredness </figcaption>
+            </figure>
+            </div>
+
+            <div class="col-lg-4 ol-md-4 col-12 mt-5">
+            <figure class="text-center">
+                <img src="images/11.jpg" class="img-fluid " width="200" height="200">
+            <figcaption> Difficulty In Breathing<br>(Severe Cases) </figcaption>
+            </figure>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="container-fluid sub_section pt-5 pb-5 " id="preventid">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>Some Steps For Prevention Against CoronaVirus </h1>
+    <div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/12.jpg" class="img-fluid " width="120" height="150">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Handwashing is one of the best ways to protect yourself and your family from getting sick. 
+                    Learn when and how you should wash your hands to stay healthy.</p>
+                </div>
+            </div>
+        </div>
+
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/13.jpg" class="img-fluid " width="150" height="180">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Putting your hand in front of your mouth to cover your cough is not advised. 
+                    When you do this, the germs will then spread onto everything you touch, including surfaces like remote controls and doorknobs, 
+                    but also things like food you serve and hands you shake.</p>
+                </div>
+            </div>
+        </div>
+
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/14.png" class="img-fluid " width="120" height="150">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Avoid close contact with people who are sick.
+                    When you are sick, keep your distance from
+                    others to protect them form getting sick too.
+                    You will help prevent others from catching your illness.</p>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/15.jpg" class="img-fluid " width="120" height="150">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Self-isolation is an effective precautionary measure to protect those around you – your family, friends, colleagues – from contracting COVID-19. 
+                    It means taking simple, common-sense steps to avoid close contact with other people as much as possible, like you would with the seasonal flu virus.</p>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/16.png" class="img-fluid " width="120" height="150">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Coronavirus Outbreak: Live Updates
+                        The new coronavirus disease outbreak first identified in China has become a pandemic. 
+                        Stay up to date with the latest updates on COVID-19.
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                    <figure class="text-center">
+                    <img src="images/17.jpg" class="img-fluid " width="120" height="150">
+            
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>To know, if you have flu, remember, flu starts with a running nose, followed by cough and fever. Very few people infected with coronavirus have reported having a running nose along with a cough. 
+                    So, if you have flu-like symptoms followed by a travel history, then only you should get yourself screened.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid pt-5 pb-5 " id="contactid">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>Contact Us</h1>
+    <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-12">
+
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label >Username</label>
+                    <input type="text" class="form-control" name="username" placeholder="Name" autocomplete="off" required>
+                </div>
+
+                <div class="form-group">
+                    <label >Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="name@example.com" required autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                    <label >Mobile No.</label>
+                    <input type="number" class="form-control" name="mobile" placeholder="Mobile No." autocomplete="off" required>
+                </div>
+
+                <div class="form-group">
+                    <label> Select Symptoms</label><br>
+                    <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+                    <input type="checkbox" class="custom-control-input" id="customcheckbox1" name="coronasym[]" value="cold">
+                    <label class="custom-control-label" for="customcheckbox1">Cold</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+                    <input type="checkbox" class="custom-control-input" id="customcheckbox2" name="coronasym[]" value="fever">
+                    <label class="custom-control-label" for="customcheckbox2">Fever</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+                    <input type="checkbox" class="custom-control-input" id="customcheckbox3" name="coronasym[]" value="breath">
+                    <label class="custom-control-label" for="customcheckbox3">Breathing Problem</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+                    <input type="checkbox" class="custom-control-input" id="customcheckbox4" name="coronasym[]" value="tired">
+                    <label class="custom-control-label" for="customcheckbox4">Feeling Weak</label>
+                    </div>
+                </div>
+  
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="msg"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+
+            </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container scrolltop float-right pr-5">
+    <i class="fa fa-arrow-up" onclick="topFunction()" id="myBtn"></i>
+</div>
+
+<footer class="mt-5">
+    <div class="footer_style text-white text-center container-fluid">
+        <p>&copy Copyright | 2020 </p>
+    </div>
+
+
+
+</footer>
+
+<script type="text/javascript">
+
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() { scrollFunction()};
+function scrollFunction(){
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+        mybuttom.style.display = "block";
+    }
+    else{
+        mybutton.style.display = "none";
+    }
+}
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+</script>
+
+</body>
+</html>
+
+
+<?php
+
+include 'dbcon.php';
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $symp = $_POST['coronasym'];
+    $msg = $_POST['username'];
+
+    $chk = "";
+
+    foreach($symp as $chk1){
+        $chk .= $chk1.",";
+    }
+
+    $insertquery = " insert into coronacase(username, email, mobile,symp, message) 
+    values('$username', '$email', '$mobile', '$chk', '$msg')";
+
+    $query = mysqli_query($con, $insertquery);
+
+    if($query){
+        ?>
+        <script>
+            alert("Inserted Successfully");
+        </script>
+        <?php
+    }else{
+        ?>
+        <script>
+            alert("Not Inserted ");
+        </script>
+        <?php
+    }
+
+}
+
+?>
