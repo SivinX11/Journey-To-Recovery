8a15b6d5a0a1c28adbc530218aac97b7a35bd4d8
Shivam Singhal <59584173+shivaamm@users.noreply.github.com>
2020-05-06 03:29:29 +0530

    Update index.php

--- covid_page/sample/index.php
+++ covid_page/sample/index.php
@@ -5,7 +5,7 @@
     <?php include 'link/links.php'; ?>
     <?php include 'css/style.php'; ?>
 </head>
-<body>
+<body onload="fetch()">
 <nav class="navbar navbar-expand-lg nav_style p-3">
   <a class="navbar-brand pl-5" href="#">CoViD-19</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,27 +48,24 @@
 </div>
 
 <!-- ********* corona updates *******-->
-<section class="updates">
+<section class="updates container-fluid">
     <div class="mb-3">
         <h3 class="text-uppercase text-center">Covid-19 Updates</h3>
     </div>
-    <div class="d-flex justify-content-around align-items-center count_style">
-        <div>
-            <h1 class="count">3000000</h1>
-            <p> Passengers</p>
-        </div>
-        <div>
-            <h1 class="count">2500</h1>
-            <p> Active Cases</p>
-        </div>
-        <div>
-            <h1 class="count">500</h1>
-            <p> Recovered</p>
-        </div>
-        <div>
-            <h1 class="count">1500</h1>
-            <p> Deaths</p>
-        </div>
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped text-center" id="tbval">
+            <tr>
+                <th>Country</th>
+                <th>Total Confirmed</th>
+                <th>Total Recovered</th>
+                <th>Total Deaths</th>
+                <th>New Recovered</th>
+                <th>New Deaths</th>
+            </tr>
+            
+        </table>
+        
+    </div>
 </section>
 
 <!-- ******* about *******-->
@@ -346,10 +343,59 @@ function topFunction(){
     document.documentElement.scrollTop = 0;
 }
 
+function fetch(){
+    $.get("https://api.covid19api.com/summary",
+        function (data){
+            console.log(data['Countries'].length);
+            var tbval = document.getElementById('tbval');
+
+            for(var i=1; i<(data['Countries'].length); i++){
+                var x = tbval.insertRow();
+                x.insertCell(0);
+
+                tbval.rows[i].cells[0].innerHTML = data['Countries'][i-1]['Country'];
+                tbval.rows[i].cells[0].style.background = '#7a4a91';
+                tbval.rows[i].cells[0].style.color = '#fff';
+
+                x.insertCell(1);
+                tbval.rows[i].cells[1].innerHTML = data['Countries'][i-1]['TotalConfirmed'];
+                tbval.rows[i].cells[1].style.background = '#4bb7d8';
+
+                x.insertCell(2);
+                tbval.rows[i].cells[2].innerHTML = data['Countries'][i-1]['TotalRecovered'];
+                tbval.rows[i].cells[2].style.background = '#4bb7d8';
+
+                x.insertCell(3);
+                tbval.rows[i].cells[3].innerHTML = data['Countries'][i-1]['TotalDeaths'];
+                tbval.rows[i].cells[3].style.background = '#4bb7d8';
+
+                x.insertCell(4);
+                tbval.rows[i].cells[4].innerHTML = data['Countries'][i-1]['NewConfirmed'];
+                tbval.rows[i].cells[4].style.background = '#4bb7d8';
+
+                x.insertCell(5);
+                tbval.rows[i].cells[5].innerHTML = data['Countries'][i-1]['NewRecovered'];
+                tbval.rows[i].cells[5].style.background = '#4bb7d8';
+
+                x.insertCell(6);
+                tbval.rows[i].cells[6].innerHTML = data['Countries'][i-1]['NewDeaths'];
+                tbval.rows[i].cells[6].style.background = '#4bb7d8';
+
+            }
+        }
+
+    );
+}
+
+
+
+
+
 </script>
 
 </body>
 </html>
+
 
 
 <?php
@@ -391,3 +437,4 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+
