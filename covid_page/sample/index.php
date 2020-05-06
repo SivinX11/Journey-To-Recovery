1252a81af7a06936f6957735e12d9a04336dc57f
Shivam Singhal <59584173+shivaamm@users.noreply.github.com>
2020-05-06 05:32:12 +0530

    Update index.php

--- covid_page/sample/index.php
+++ covid_page/sample/index.php
@@ -27,6 +27,12 @@
         <a class="nav-link" href="#preventid">Preventions</a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="worldlivestats.php">World Live</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="indialivestats.php">India Live</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="#contactid">Contact Us</a>
       </li>
     </ul>
@@ -47,27 +53,6 @@
         </div>
 </div>
 
-<!-- ********* corona updates *******-->
-<section class="updates container-fluid">
-    <div class="mb-3">
-        <h3 class="text-uppercase text-center">Covid-19 Updates</h3>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped text-center" id="tbval">
-            <tr>
-                <th>Country</th>
-                <th>Total Confirmed</th>
-                <th>Total Recovered</th>
-                <th>Total Deaths</th>
-                <th>New Confirmed</th>
-                <th>New Recovered</th>
-                <th>New Deaths</th>
-            </tr>
-            
-        </table>
-        
-    </div>
-</section>
 
 <!-- ******* about *******-->
 <div class="container-fluid sub_section pt-5 pb-5 " id="aboutid">
@@ -344,49 +329,6 @@ function topFunction(){
     document.documentElement.scrollTop = 0;
 }
 
-function fetch(){
-    $.get("https://api.covid19api.com/summary",
-        function (data){
-            console.log(data['Countries'].length);
-            var tbval = document.getElementById('tbval');
-
-            for(var i=1; i<(data['Countries'].length); i++){
-                var x = tbval.insertRow();
-                x.insertCell(0);
-
-                tbval.rows[i].cells[0].innerHTML = data['Countries'][i-1]['Country'];
-                tbval.rows[i].cells[0].style.background = '#7a4a91';
-                tbval.rows[i].cells[0].style.color = '#fff';
-
-                x.insertCell(1);
-                tbval.rows[i].cells[1].innerHTML = data['Countries'][i-1]['TotalConfirmed'];
-                tbval.rows[i].cells[1].style.background = '#4bb7d8';
-
-                x.insertCell(2);
-                tbval.rows[i].cells[2].innerHTML = data['Countries'][i-1]['TotalRecovered'];
-                tbval.rows[i].cells[2].style.background = '#4bb7d8';
-
-                x.insertCell(3);
-                tbval.rows[i].cells[3].innerHTML = data['Countries'][i-1]['TotalDeaths'];
-                tbval.rows[i].cells[3].style.background = '#4bb7d8';
-
-                x.insertCell(4);
-                tbval.rows[i].cells[4].innerHTML = data['Countries'][i-1]['NewConfirmed'];
-                tbval.rows[i].cells[4].style.background = '#4bb7d8';
-
-                x.insertCell(5);
-                tbval.rows[i].cells[5].innerHTML = data['Countries'][i-1]['NewRecovered'];
-                tbval.rows[i].cells[5].style.background = '#4bb7d8';
-
-                x.insertCell(6);
-                tbval.rows[i].cells[6].innerHTML = data['Countries'][i-1]['NewDeaths'];
-                tbval.rows[i].cells[6].style.background = '#4bb7d8';
-
-            }
-        }
-
-    );
-}
 
 
 
